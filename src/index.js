4ed54125264693f2789339540cd1415a426367ee
Koki Shibata <koki.shibata@cloud-consulting.asia>
2022-03-15 12:13:47 +0800

    completed tutorial

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {
+	BrowserRouter,
+	Routes,
+	Route
+} from 'react-router-dom'
+import Expenses from './routes/expenses'
+import Invoices from './routes/invoices'
+import Invoice from './routes/invoice'
 
+const rootElement = document.getElementById('root')
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<BrowserRouter>
+		<Routes>
+			<Route path='/' element={<App/>}>
+				<Route path='expenses' element={<Expenses/>}/>
+				<Route path='invoices' element={<Invoices/>}>
+					<Route index element={
+						<main style={{ padding: "1rem" }}>
+							<p>Select an invoice</p>
+						</main>
+					}/>
+					<Route path=":invoiceId" element={<Invoice/>}/>
+				</Route>
+			</Route>
+			<Route
+				path="*"
+				element={
+					<main style={{ padding: '1rem' }}>
+						<p>There's nothing here!</p>
+					</main>
+				}
+			/>
+		</Routes>
+	</BrowserRouter>,
+  	rootElement
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
